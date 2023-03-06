cf20639c3f331a5d7ce037fae15fbda090d333f0
ZejunZhou <zzhou443>
2023-03-06 17:37:15 -0600

    add ejs file and render to the home route

--- EJS/EJS.md
+++ EJS/EJS.md
@@ -1,1 +1,19 @@
 # EJS (embed javascript template)
+
+## Installation
+npm i ejs
+---
+## Set up
+app use ejs as view engine
+```
+app.use("view engine", "ejs");
+```
+---
+## Use EJS with Express
+
+**Format**: <%= EJS %>
+
+1. set up *views* directory
+2. create ejs file (same as html)
+3. response.render("index", {"days":day}) **"index"** is refering ot index.ejs file and **"days"** is the ejs argument in ejs file, **day** is the value
+
--- app.js
+++ app.js
@@ -2,11 +2,38 @@ const express = require("express");
 const body_parser = require("body-parser");
 const { request } = require("http");
 const app = express();
+// app use ejs as view engine
+app.set("view engine", "ejs");
+
 app.listen("3000");
 app.get("/", (request, response) =>{
-    var date = new Date()
-    console.log(date.getDate())
-    
-    response.send("hello")
+    var date = new Date();
+    day_number = date.getDay();
+
+    day = calculate_day(day_number);
+
+
+    response.render("index", {"days":day})
 }) 
 console.log("app ls listen on port 3000")
+
+const calculate_day = (day_number) => {
+    switch (day_number){
+        case 0:
+            return "Sunday";
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+        default:
+            return "Invalid date due to error"
+    }
+}
