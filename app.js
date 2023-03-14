dbf2fa82e70c622dadb7f26aa9372373c73b8292
ZejunZhou <zzhou443>
2023-03-14 11:12:48 -0500

    add MongoDB to save add to do event

--- app.js
+++ app.js
@@ -1,37 +1,100 @@
 const express = require("express");
 const body_parser = require("body-parser");
-const { request } = require("express");
-//const { request } = require("http");
+const mongoose = require("mongoose");
+// brew services run mongodb-community (use this command to start mongodb server)
 const app = express();
 app.use(body_parser.urlencoded({ extended: true }));
 
 // app use ejs as view engine
 app.set("view engine", "ejs");
 app.listen("3000");
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
 
-// create a dynamic array for storing item
-let items = ["Buy Grocery", "Go to School"]
 
+// connect to mongoDB
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const todolist_schema = {
+    name: {
+        type:String,
+        required: true
+    }
+}
+// create mongoose data model
+const Item_Model = mongoose.model("item", todolist_schema);
+
+const Item_Document1  = new Item_Model ({
+    name: "Get Water"
+})
+
+const Item_Document2  = new Item_Model ({
+    name: "Make dinner"
+})
+
+// save item document in a list
+const document_list = [Item_Document1, Item_Document2]
+
+// use insert many to place into database
+// Item_Model.insertMany(document_list)
+//   .then(docs => {
+//     console.log(docs.length + ' docs saved to database.');
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
+
+
+// create a dynamic array for work list
+let workItem = [];
+
+/**Render to home page */
 app.get("/", (request, response) =>{
-    let date = new Date();
-    //create a object to render today's date
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    var day = date.toLocaleDateString("en-US", options);
-    response.render("index", {"days":day, "items":items})
+    
+    Item_Model.find()
+    .then((docs) => {
+        
+        let date = new Date();
+        let options = {
+            weekday: "long",
+            day: "numeric",
+            month: "long"
+        };
+        var day = date.toLocaleDateString("en-US", options);
+        if (docs.length == 0) {
+            response.render("index", {"title":day, "items":[{name:"Let's get Started!"}], "postType": "home"})
+        } else {
+            response.render("index", {"title":day, "items": docs, "postType": "home"})
+        }
+    })
+    .catch((err) => console.log(err));
+    
 }) 
 // callback function when recieve the post request from the form
 app.post("/", (request, response) =>{
-    let newItem = request.body.newItem;
-    items.push(newItem);
-    /**when we receive the post request from the form
-    we save the variable newItem and redirect to the home route
-    Otherwise, there can be a issue when response render **/
-    response.redirect("/")
+    // case1: when button's value is work, render at /Worklist
+    if (request.body.button == "work"){
+       let newItem = request.body.newItem;
+       workItem.push(newItem);
+       response.redirect("/Worklist");
+
+    // caes2: home case, redner at / 
+    }else{
+        let newItem = request.body.newItem;
+        const Item_Insert = new Item_Model({
+            name: newItem
+        })
+
+        Item_Insert.save()
+
+        //items.push(newItem);
+        /**when we receive the post request from the form
+        we save the variable newItem and redirect to the home route
+        Otherwise, there can be a issue when response render **/
+        response.redirect("/")
+    }
 })
 
+/**Render to Worklist */
+app.get("/Worklist", (request, response) =>{
+    response.render("index", {"title":"Work List", "items":workItem, "postType": "work"});
+})
